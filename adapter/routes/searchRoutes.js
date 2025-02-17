2e0b1df516dca3110169197b01703b7138e44984
Kushagra Gupta <kushagragupta625@gmail.com>
2025-02-18 02:09:25 +0530

    minor fixes in /search

--- adapter/routes/searchRoutes.js
+++ adapter/routes/searchRoutes.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const searchController = require('../controllers/searchController');
+const express = require("express");
+const searchController = require("../controllers/searchController");
 const router = express.Router();
 
-router.post('/', searchController.handleSearchRequest);
+router.post("/", searchController.handleSearchRequest);
 
 module.exports = router;
